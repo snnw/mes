640ec0a9ea235943cce34b0c2c1d68a0d0940e54
Jan Nieuwenhuizen <janneke@gnu.org>
2019-01-05 12:23:49 +0100

    mescc: Mes C Library: Support GNU Awk: Add wchar_t.
    * include/sys/types.h (wchar_t): New typedef.

--- lib/stdlib/mbstowcs.c
+++ lib/stdlib/mbstowcs.c
@@ -20,9 +20,6 @@
 
 #include <stdlib.h>
 
-#if !__MESC__
-typedef char wchar_t[];
-
 size_t
 mbstowcs (wchar_t *wstring, char const *string,
           size_t size)
@@ -34,4 +31,3 @@ mbstowcs (wchar_t *wstring, char const *string,
   strcpy (wstring, string);
   return strlen (string);
 }
-#endif
