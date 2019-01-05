e81e48c701b8a96f54bc3bc81f50cae5451cce31
Jan Nieuwenhuizen <janneke@gnu.org>
2019-01-05 11:23:54 +0100

    mescc: Mes C Library: Support GNU Tar: Add getgrgid, getgrnam, setgrent stub.
    * include/grp.h: New file.
    * lib/stub/getgrgid.c: New file.
    * lib/stub/getgrnam.c: New file.
    * lib/stub/setgrent.c: New file.
    * lib/libc+gnu.c: Include them.

---
+++ include/grp.h
@@ -0,0 +1,43 @@
+/* -*-comment-start: "//";comment-end:""-*-
+ * GNU Mes --- Maxwell Equations of Software
+ * Copyright © 2019 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ *
+ * This file is part of GNU Mes.
+ *
+ * GNU Mes is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * GNU Mes is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __MES_GRP_H
+#define __MES_GRP_H 1
+
+#if WITH_GLIBC
+#undef __MES_GRP_H
+#include_next <pwd.h>
+#else  // ! WITH_GLIBC
+
+struct group
+{
+  char *gr_name;
+  gid_t gr_gid;
+  char **gr_mem;
+};
+
+struct group *getgrent (void);
+void endgrent (void);
+void setgrent (void);
+struct group *getgrgid (gid_t gid);
+struct group *getgrnam (char const *name);
+
+#endif  // ! WITH_GLIBC
+
+#endif // __MES_GRP_H
