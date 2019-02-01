eeda1f34501e26b80c5c04894731c2ca127ab96b
Jan Nieuwenhuizen <janneke@gnu.org>
2019-02-01 09:29:39 +0100

    mescc: Mes C Library: Support gcc-4.6.4: Add stdint [U]SHRT macros.
    * include/stdint.h (SHRT_MAX, SHRT_MIN, USHRT_MAX, USHRT_MIN): New
    macro.

--- include/stdint.h
+++ include/stdint.h
@@ -103,6 +103,12 @@ typedef long uintptr_t;
 
 #define INT_MIN -2147483648
 #define INT_MAX 2147483647
+
+#define SHRT_MAX INT16_MAX
+#define SHRT_MIN INT16_MIN
+#define USHRT_MAX UINT16_MAX
+#define USHRT_MIN UINT16_MIN
+
 #if __i386__
 #define LONG_MIN INT_MIN
 #define LONG_MAX INT_MAX
