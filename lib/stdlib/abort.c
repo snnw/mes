4360fb5c2351622abc904dfbab578a16b7de1dd5
Jan Nieuwenhuizen <janneke@gnu.org>
2019-03-03 18:08:38 +0100

    ARM: Mes C Library: Support abort.
    * lib/stdlib/abort.c (abort): Support Arm.
    * lib/arm-mes/arm.M1 (wfi): New macro.

--- lib/stdlib/abort.c
+++ lib/stdlib/abort.c
@@ -1,6 +1,6 @@
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
- * Copyright © 2018 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2018,2019 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
  *
  * This file is part of GNU Mes.
  *
@@ -18,8 +18,16 @@
  * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+
 void
 abort (void)
 {
+#if __arm__
+  asm ("wfi");
+#else
   asm ("hlt");
+#endif
+  // not reached
+  exit (77);
 }
