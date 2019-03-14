1d66bbd78e594e75251e4c5629d8a04d2226836c
Danny Milosavljevic <dannym@scratchpost.org>
2019-03-14 23:43:37 +0100

    Fix dtoab.
    * lib/mes/dtoab.c (dtoab): Fix it.
    * lib/tests/mes/90-dtoab.c (main): Fix test.
    * build-aux/check-mescc.sh (XFAIL_TESTS: Remove it.

--- build-aux/check-mescc.sh
+++ build-aux/check-mescc.sh
@@ -253,12 +253,6 @@ lib/tests/scaffold/a0-call-trunc-int.c
     fi
 fi
 
-if test $mes_cpu = x86; then
-    XFAIL_TESTS="$XFAIL_TESTS
-lib/tests/mes/90-dtoab.c
-"
-fi
-
 if test $mes_cpu = x86_64; then
     XFAIL_TESTS="$XFAIL_TESTS
 lib/tests/stdio/70-printf-stdarg.c
@@ -268,6 +262,7 @@ fi
 if test $compiler = gcc; then
     XFAIL_TESTS="$XFAIL_TESTS
 lib/tests/mes/90-abtod.c
+lib/tests/mes/90-dtoab.c
 "
 
     if test $mes_cpu = x86; then
